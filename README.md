34b7dfa476cbe2b497a2ced68bc4e5276827975e
SirKleyton <cleyton_ferr@hotmail.com>
2022-06-30 13:44:11 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-35acpe
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-35acpe)
---
+++ index.js
@@ -0,0 +1,13 @@
+const somosCasados = true
+const anoNascimento  = 1986
+const media = (7+8+9+10) /4
+const nome = "Cleiton"
+let idade = null
+const terceiroFilho =
+const numeroGrande = 2342342331654654n
+const numeroGrande2 =  Bigint("21324564122345n")
+const meuSimbolo = new Symmbol(
+  consy meuObjeto ={
+   curso "Estrutura de Dados"
+instrutores ['Marcia ' . 'Cleiton]
+  }
